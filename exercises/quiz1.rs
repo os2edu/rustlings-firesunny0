928edbedd825fa72d2a0ab40dfd92035f6c01238
wjy_laptop14 <firesunny@hotmail.com>
2022-11-05 00:49:40 +0800

    update: finish exercises/structs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: isize) -> isize {
+    if num <= 40 {
+        return num * 2;
+    } else {
+        return num;
+    }
+}
 
 // Don't modify this function!
 #[test]
